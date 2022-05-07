c5b565b598e35af937b391c5597181af9ca217af
LawnMeower <s_schmidt1993@outlook.de>
2022-05-07 04:45:25 +0200

    created MemCompareResult class

--- include/MemCompare.h
+++ include/MemCompare.h
@@ -2,9 +2,90 @@
 #include"definitions.h"
 #include"MemDump.h"
 #include<functional>
+#include<iostream>
 
 namespace Xertz
 {
+	class MemCompareResult
+	{
+	private:
+		void* _results = nullptr;
+		std::wstring _filePath;
+		void* _fileHeader = malloc(16);
+		unsigned short _iteration = 0;
+		unsigned short _addressWidth = 4;
+		unsigned long long _resultCount = 0;
+		bool _cached = false;
+		unsigned int _magic = 0x2e6d6372; //.mcr (LE)
+
+		bool SaveResults()
+		{
+			if (Xertz::SaveBinary(_filePath, _fileHeader, 16))
+			{
+				return Xertz::SaveBinary(_filePath, _results, _resultCount * _addressWidth, true);
+			}
+			return false;
+		}
+
+		bool LoadResults()
+		{
+			if (Xertz::LoadBinary(_filePath, _fileHeader, 16, 0))
+			{
+				if (*(unsigned int*)_fileHeader == _magic)
+				{
+					_iteration = *((unsigned short*)_fileHeader + 2);
+					_addressWidth = *((unsigned short*)_fileHeader + 3);
+					_resultCount = *((unsigned long long*)_fileHeader + 1);
+					_results = malloc(_resultCount * _addressWidth);
+					return Xertz::LoadBinary(_filePath, _results, _resultCount * _addressWidth, 16);
+				}
+			}
+			return false;
+		}
+
+	public:
+		MemCompareResult(bool cached, unsigned short addressWidth, unsigned long long resultCount, short iteration, void* results = nullptr, std::wstring dirPath = L"")
+		{
+			_cached = cached;
+			_iteration = iteration;
+			_resultCount = resultCount;
+			_addressWidth = addressWidth;
+			_results = results;
+			_filePath = dirPath;
+			_filePath.append(L"\\").append(std::to_wstring(iteration)).append(L".mcr");
+
+			*(unsigned int*)_fileHeader = _magic;
+			*((unsigned short*)_fileHeader + 2) = _iteration;
+			*((unsigned short*)_fileHeader + 3) = _addressWidth;
+			*((unsigned long long*)_fileHeader + 1) = _resultCount;
+
+			if (cached && results == nullptr)
+			{
+				LoadResults();
+			}
+			else if (cached && results != nullptr)
+			{
+				SaveResults();
+			}
+			else
+			{
+				_results = results;
+			}
+		}
+
+		~MemCompareResult()
+		{
+			free(_results);
+			free(_fileHeader);
+		}
+
+		void* GetResults() { return _results; }
+		unsigned short GetIteration() { return _iteration; }
+		unsigned short GetAddressWidth() { return _addressWidth; }
+		unsigned long long GetResultsCount() { return _resultCount; }
+		bool IsCached() { return _cached; }
+	};
+
 	class MemCompare
 	{
 	private:
@@ -63,12 +144,6 @@ namespace Xertz
 
 
 
-		struct Result
-		{
-			bool _cached = false;
-			void* _results = nullptr;
-			std::wstring _filePath;
-		};
 	};
 }
 
